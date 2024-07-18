56704019a036db5dacd8afa99991d86d459859cb
Joshua Kaiser <jmk@Mini-von-Joshua.fritz.box>
2024-07-18 15:43:43 +0200

    frontend 2

--- frontend/src/components/pages/ContactsList.page.jsx
+++ frontend/src/components/pages/ContactsList.page.jsx
@@ -27,4 +27,13 @@ const ContactsListPage = () => {
           <ContactCard 
             key={contact.id} 
             contact={contact} 
-            
+            onDelete={handleDelete}
+            onView={() => navigate(`/contacts/${contact.id}`)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ContactsListPage;
